174ed99af6b4ef7ffebbb6204a4d2984e8d33d22
moliusha <moliusha@Ubuntu22.04.1.myguest.virtualbox.org>
2023-04-13 17:25:53 -0700

    test

--- main.py
+++ main.py
@@ -35,7 +35,7 @@ def final_process(data1: List[int], data2: List[int]) -> List[int]:
     """TODO: Document this function. What does it do? What are the inputs and outputs?"""
     return np.mean([x - y for x, y in zip(data1, data2)])
 
-offload_url = 'http://192.168.4.74:5000' # TODO: Change this to the IP address of your server
+offload_url = 'http://192.168.143.198:5000' # TODO: Change this to the IP address of your server
 
 def run(offload: Optional[str] = None) -> float:
     """Run the program, offloading the specified function(s) to the server.
@@ -55,6 +55,7 @@ def run(offload: Optional[str] = None) -> float:
         def offload_process1(data):
             nonlocal data1
             # TODO: Send a POST request to the server with the input data
+	    response = request.post(f"{offload_url}/process1",jason=data)
             data1 = response.json()
         thread = threading.Thread(target=offload_process1, args=(data,))
         thread.start()
@@ -67,10 +68,38 @@ def run(offload: Optional[str] = None) -> float:
         #   Make sure to cite any sources you use to answer this question.
     elif offload == 'process2':
         # TODO: Implement this case
-        pass
+        data2 = None
+	def offload_process2(data):
+		nonlocal data2
+		response = request.post(f"{offload_url}/process2", jason=data)
+		data2 = response.json()
+	thread = threading.Thread(target=offload_process1, args=(data,))
+        thread.start()
+        data1 = process1(data)
+        thread.join()
     elif offload == 'both':
         # TODO: Implement this case
-        pass
+        data1 = None
+        def offload_process1(data):
+            nonlocal data1
+	    response = request.post(f"{offload_url}/process1",jason=data)
+            # TODO: Send a POST request to the server with the input data
+            data1 = response.json()
+        thread = threading.Thread(target=offload_process1, args=(data,))
+        thread.start()
+        data2 = process2(data)
+        thread.join()
+
+	data2 = None
+        def offload_process2(data):
+                nonlocal data2
+                response = request.post(f"{offload_url}/process2", jason=data)
+                data2 = response.json()
+        thread = threading.Thread(target=offload_process1, args=(data,))
+        thread.start()
+        data1 = process1(data)
+        thread.join()
+
 
     ans = final_process(data1, data2)
     return ans 
@@ -79,14 +108,23 @@ def main():
     # TODO: Run the program 5 times for each offloading mode, and record the total execution time
     #   Compute the mean and standard deviation of the execution times
     #   Hint: store the results in a pandas DataFrame, use previous labs as a reference
-
-
+	offloading = [None, "Process1", "Process2", "Both"]
+	times = []
+	start_time = time.time()
+	for mode in offloading:
+		for i in range(5):
+			start = time.time()
+			run(mode)
+			end = time.time()
+			times.append(end - start)
+			print(f"{mode}, mean:{np.mean(times)}, standard deviation:{np.std(times)}")
+	df = pd.DataFrame(data, columns=['Mode', "Mean", "Standard Deviation"])
     # TODO: Plot makespans (total execution time) as a bar chart with error bars
     # Make sure to include a title and x and y labels
-
+	bar = px.bar(df, x="Mode", y="Runtime", error_y="std_time", title="Makespans")
 
     # TODO: save plot to "makespan.png"
-
+	bar.write_image("makespan.png")
 
     # Question 4: What is the best offloading mode? Why do you think that is?
     # Question 5: What is the worst offloading mode? Why do you think that is?
--- server.py
+++ server.py
@@ -11,3 +11,19 @@ def index():
 
 # TODO: Create a flask app with two routes, one for each function.
 # The route should get the data from the request, call the function, and return the result.
+@app.route('/process1', methods=['POST'])
+def process1_route():
+    data = request.get_json()
+    result = process1(data)
+    return jsonify({'result': result})
+
+
+@app.route('/process2', methods=['POST'])
+def process2_route():
+    data = request.get_json()
+    result = process2(data)
+    return jsonify({'result': result})
+  
+    
+if __name__ == '__main__':
+    app.run(debug=True)
